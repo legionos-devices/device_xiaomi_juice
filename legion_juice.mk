b8e32ddcada2340b3e5e13c81e0e7673d3e319c4
TyraLizQ10 <nathanielquitua@gmail.com>
2021-07-28 14:49:47 +0000

    Juice: Add necessary Legion overlay

--- legion_juice.mk
+++ legion_juice.mk
@@ -23,3 +23,7 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ2A.210405.005/7181113:user/release-keys"
+
+# LegionOS Device Maintainers
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.maintainer.name=NathanielQuitua
